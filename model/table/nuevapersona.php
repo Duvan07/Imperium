8ed35dd40a33b364aeb2355214f8097c178e6849
USUARIO <USUARIO@10.72.134.75>
2017-05-25 12:15:01 -0500

    1. se agrego el DAO de la tabla Nuevousuario

---
+++ model/table/nuevapersona.php
@@ -0,0 +1,294 @@
+<?php
+
+class nuevapersona {
+  private $id;
+  private $cedula;
+  private $nombres;
+  private $apellidos;
+  private $genero;
+  private $fenacimiento;
+  private $edad;
+  private $lugarnacimiento;
+  private $nacionalidad;
+  private $lugarresidencia;
+  private $direccionresidencia;
+  private $notelefono;
+  private $correo;
+  private $fecingresoempresa;
+  private $antiguedad;
+  private $cargo;
+  private $centrotrabajo;
+  private $area;
+  private $turno;
+  private $salario;
+  private $tipovinculacion;
+  private $arl;
+  private $eps;
+  private $afp;
+  private $escolaridad;
+  private $profesion;
+  private $estadocivi;
+  private $raza;
+  private $nohijos;
+  private $otrasperacargo;
+  private $estratosocial;
+  private $concentimientoformado;
+  
+  function getId() {
+    return $this->id;
+  }
+
+  function getCedula() {
+    return $this->cedula;
+  }
+
+  function getNombres() {
+    return $this->nombres;
+  }
+
+  function getApellidos() {
+    return $this->apellidos;
+  }
+
+  function getGenero() {
+    return $this->genero;
+  }
+
+  function getFenacimiento() {
+    return $this->fenacimiento;
+  }
+
+  function getEdad() {
+    return $this->edad;
+  }
+
+  function getLugarnacimiento() {
+    return $this->lugarnacimiento;
+  }
+
+  function getNacionalidad() {
+    return $this->nacionalidad;
+  }
+
+  function getLugarresidencia() {
+    return $this->lugarresidencia;
+  }
+
+  function getDireccionresidencia() {
+    return $this->direccionresidencia;
+  }
+
+  function getNotelefono() {
+    return $this->notelefono;
+  }
+
+  function getCorreo() {
+    return $this->correo;
+  }
+
+  function getFecingresoempresa() {
+    return $this->fecingresoempresa;
+  }
+
+  function getAntiguedad() {
+    return $this->antiguedad;
+  }
+
+  function getCargo() {
+    return $this->cargo;
+  }
+
+  function getCentrotrabajo() {
+    return $this->centrotrabajo;
+  }
+
+  function getArea() {
+    return $this->area;
+  }
+
+  function getTurno() {
+    return $this->turno;
+  }
+
+  function getSalario() {
+    return $this->salario;
+  }
+
+  function getTipovinculacion() {
+    return $this->tipovinculacion;
+  }
+
+  function getArl() {
+    return $this->arl;
+  }
+
+  function getEps() {
+    return $this->eps;
+  }
+
+  function getAfp() {
+    return $this->afp;
+  }
+
+  function getEscolaridad() {
+    return $this->escolaridad;
+  }
+
+  function getProfesion() {
+    return $this->profesion;
+  }
+
+  function getEstadocivi() {
+    return $this->estadocivi;
+  }
+
+  function getRaza() {
+    return $this->raza;
+  }
+
+  function getNohijos() {
+    return $this->nohijos;
+  }
+
+  function getOtrasperacargo() {
+    return $this->otrasperacargo;
+  }
+
+  function getEstratosocial() {
+    return $this->estratosocial;
+  }
+
+  function getConcentimientoformado() {
+    return $this->concentimientoformado;
+  }
+
+  function setId($id) {
+    $this->id = $id;
+  }
+
+  function setCedula($cedula) {
+    $this->cedula = $cedula;
+  }
+
+  function setNombres($nombres) {
+    $this->nombres = $nombres;
+  }
+
+  function setApellidos($apellidos) {
+    $this->apellidos = $apellidos;
+  }
+
+  function setGenero($genero) {
+    $this->genero = $genero;
+  }
+
+  function setFenacimiento($fenacimiento) {
+    $this->fenacimiento = $fenacimiento;
+  }
+
+  function setEdad($edad) {
+    $this->edad = $edad;
+  }
+
+  function setLugarnacimiento($lugarnacimiento) {
+    $this->lugarnacimiento = $lugarnacimiento;
+  }
+
+  function setNacionalidad($nacionalidad) {
+    $this->nacionalidad = $nacionalidad;
+  }
+
+  function setLugarresidencia($lugarresidencia) {
+    $this->lugarresidencia = $lugarresidencia;
+  }
+
+  function setDireccionresidencia($direccionresidencia) {
+    $this->direccionresidencia = $direccionresidencia;
+  }
+
+  function setNotelefono($notelefono) {
+    $this->notelefono = $notelefono;
+  }
+
+  function setCorreo($correo) {
+    $this->correo = $correo;
+  }
+
+  function setFecingresoempresa($fecingresoempresa) {
+    $this->fecingresoempresa = $fecingresoempresa;
+  }
+
+  function setAntiguedad($antiguedad) {
+    $this->antiguedad = $antiguedad;
+  }
+
+  function setCargo($cargo) {
+    $this->cargo = $cargo;
+  }
+
+  function setCentrotrabajo($centrotrabajo) {
+    $this->centrotrabajo = $centrotrabajo;
+  }
+
+  function setArea($area) {
+    $this->area = $area;
+  }
+
+  function setTurno($turno) {
+    $this->turno = $turno;
+  }
+
+  function setSalario($salario) {
+    $this->salario = $salario;
+  }
+
+  function setTipovinculacion($tipovinculacion) {
+    $this->tipovinculacion = $tipovinculacion;
+  }
+
+  function setArl($arl) {
+    $this->arl = $arl;
+  }
+
+  function setEps($eps) {
+    $this->eps = $eps;
+  }
+
+  function setAfp($afp) {
+    $this->afp = $afp;
+  }
+
+  function setEscolaridad($escolaridad) {
+    $this->escolaridad = $escolaridad;
+  }
+
+  function setProfesion($profesion) {
+    $this->profesion = $profesion;
+  }
+
+  function setEstadocivi($estadocivi) {
+    $this->estadocivi = $estadocivi;
+  }
+
+  function setRaza($raza) {
+    $this->raza = $raza;
+  }
+
+  function setNohijos($nohijos) {
+    $this->nohijos = $nohijos;
+  }
+
+  function setOtrasperacargo($otrasperacargo) {
+    $this->otrasperacargo = $otrasperacargo;
+  }
+
+  function setEstratosocial($estratosocial) {
+    $this->estratosocial = $estratosocial;
+  }
+
+  function setConcentimientoformado($concentimientoformado) {
+    $this->concentimientoformado = $concentimientoformado;
+  }
+
+
+}
